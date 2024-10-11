c208eb62b383ad1938dd894b311bf2c2aa1af732
Mohanakahnan S. <UT01146@unicomtic.com>
2024-10-11 12:40:20 +0530

    question 13

---
+++ FitnessProgramManagementSystem_V2/FitnessProgramManagementSystem_V2/FitnessProgramRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgramRepository
+    {
+        string connectionString = "Server=(localdb)\\MSSQLLocalDB;Initial Catalog=FitnessProgramManagement2;";
+
+        //1 insert new Finess program
+        public void InsertNewFitnessprogram(FitnessPrograms fitnessProgram)
+        {
+            string query = "INSERT INTO FitnessPrograms (Title, Duration, Price) VALUES (@Title,@Duration,@Price);";
+
+            try
+            {
+                using (SqlConnection Connection = new SqlConnection(connectionString))
+                {
+                    Connection.Open();
+                    using (SqlCommand Command = new SqlCommand(query, Connection))
+                    {
+                        Command.Parameters.AddWithValue("@Title", fitnessProgram.Title);
+                        Console.WriteLine("Title Added");
+                        Command.Parameters.AddWithValue("@Duration", fitnessProgram.Duration);
+                        Command.Parameters.AddWithValue("@Price", fitnessProgram.Price);
+                        Command.ExecuteNonQuery();
+                    };
+                }
+                Console.WriteLine("Fitness program inserted successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while inserting" + ex.Message);
+            }
+
+        }
+
+
+    }
+}
+
---
+++ FitnessProgramManagementSystem_V2/FitnessProgramManagementSystem_V2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+    }
+
+}
+
+
+
+ 
+    
